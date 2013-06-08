22d6d17c37dd53a4f254f889d9a78b602f75a686
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-06-08 09:40:53 -0400

    Add function to calculate position one time step in the future

--- simulation.rb
+++ simulation.rb
@@ -12,7 +12,12 @@ class Simulation
 		@kunai.velocity.x += acceleration.x*@time_step
 		@kunai.velocity.y += acceleration.y*@time_step
 		@kunai.velocity.z += acceleration.z*@time_step
-		return Triple.new(@kunai.velocity.x, @kunai.velocity.y, @kunai.velocity.z)
+	end
+
+	def evolve_position(acceleration)
+		@kunai.position.x += (@kunai.velocity.x*@time_step + 0.5*acceleration.x*@time_step*@time_step)
+		@kunai.position.y += (@kunai.velocity.y*@time_step + 0.5*acceleration.y*@time_step*@time_step)
+		@kunai.position.z += (@kunai.velocity.z*@time_step + 0.5*acceleration.z*@time_step*@time_step)
 	end
 
 end
--- spec/simulation_spec.rb
+++ spec/simulation_spec.rb
@@ -58,4 +58,39 @@ describe Simulation do
 		@simulation.kunai.velocity.z.should eql 1.0
 	end
 
+	it "should be able to calculate the next position for no acceleration" do
+		@simulation.kunai.position.x.should eql 20.0
+		@simulation.evolve_position(Triple.new(0, 0, 0))
+		@simulation.kunai.position.x.should eql 19.975
+	end
+
+	it "should be able to calculate the next position for x-acceleration" do
+		@simulation.kunai.position.x.should eql 20.0
+		@simulation.evolve_position(Triple.new(1000, 0, 0))
+		@simulation.kunai.position.x.should eql 19.9755
+	end
+
+	it "should be able to calculate the next position for y-acceleration" do
+		@simulation.kunai.position.y.should eql 0.0
+		@simulation.evolve_position(Triple.new(0, 1000, 0))
+		@simulation.kunai.position.y.should eql 0.0005
+	end
+
+	it "should be able to calculate the next position for z-acceleration" do
+		@simulation.kunai.position.z.should eql 0.0
+		@simulation.evolve_position(Triple.new(0, 0, 1000))
+		@simulation.kunai.position.z.should eql 0.0005
+	end
+
+	it "should be able to calculate the next position for a general acceleration" do
+		@simulation.kunai.position.x.should eql 20.0
+		@simulation.kunai.position.y.should eql 0.0
+		@simulation.kunai.position.z.should eql 0.0
+		@simulation.evolve_position(Triple.new(1000, 1000, 1000))
+		@simulation.kunai.position.x.should eql 19.9755
+		@simulation.kunai.position.y.should eql 0.0005
+		@simulation.kunai.position.z.should eql 0.0005
+	end
+
+
 end
