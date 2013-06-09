206b4b1973028c0360bd1334ab44d91cf9916fa5
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-06-09 18:28:49 -0400

    Clean up triple_spec.rb

--- spec/triple_spec.rb
+++ spec/triple_spec.rb
@@ -77,65 +77,46 @@ describe Triple do
 		specify { @zero_triple.r_hat.magnitude.should eql 0.0 }
 	end
 
-	it "should be able to sum a zero and non-zero triple as if they were vectors" do
-		specify { @triple.add_to(@zero_triple).x.should eql 1.0}
-		@zero_triple.add_to(@triple).y.should eql 2.0
-		@triple.add_to(@zero_triple).z.should eql -3.0
+	context "the add_to method should sum a pair of triples" do
+		specify { @triple.add_to(@zero_triple).is_equal_to?(@triple).should eql true }
+		specify { @negative_triple.add_to(@zero_triple).is_equal_to?(@negative_triple).should eql true }
+		specify { @zero_triple.add_to(@zero_triple).is_equal_to?(@zero_triple).should eql true }
+
+		specify { @triple.add_to(@negative_triple).x.should eql 0.9 }
+		specify { @triple.add_to(@negative_triple).y.should eql 1.1 }
+		specify { @triple.add_to(@negative_triple).z.should eql -13.0 }
+
+		specify { @negative_triple.add_to(@triple).x.should eql 0.9 }
+		specify { @negative_triple.add_to(@triple).y.should eql 1.1 }
+		specify { @negative_triple.add_to(@triple).z.should eql -13.0 }
 	end
 
-	it "should sum two triples as vectors" do
-		@triple.add_to(@negative_triple).x.should eql 0.9
-		@triple.add_to(@negative_triple).y.should eql 1.1
-		@triple.add_to(@negative_triple).z.should eql -13.0
+	context "the times_constant method should multiply each value of a triple by a constant" do
+		specify { @triple.times_constant(5).is_equal_to?(Triple.new(5.0, 10.0, -15.0)).should eql true }
+		specify { @triple.times_constant(-5).is_equal_to?(Triple.new(-5.0, -10.0, 15.0)).should eql true }
+		specify { @triple.times_constant(0).is_equal_to?(@zero_triple).should eql true }
+
+		specify { @negative_triple.times_constant(1.0).is_equal_to?(Triple.new(-0.1, -0.9, -10.0)).should eql true }
+		specify { @negative_triple.times_constant(0.000).is_equal_to?(@zero_triple).should eql true }
+
+		specify { @zero_triple.times_constant(6.0).is_equal_to?(@zero_triple).should eql true }
+		specify { @zero_triple.times_constant(0.0).is_equal_to?(@zero_triple).should eql true }
 	end
 
-	it "should have an associative add_to function" do
-		@triple.add_to(@negative_triple).x.should eql 0.9
-		@triple.add_to(@negative_triple).y.should eql 1.1
-		@triple.add_to(@negative_triple).z.should eql -13.0
-		@negative_triple.add_to(@triple).x.should eql 0.9
-		@negative_triple.add_to(@triple).y.should eql 1.1
-		@negative_triple.add_to(@triple).z.should eql -13.0
-	end
+	context "the is_equal_to? method should compare two triples" do
+		before(:all) do
+			@duplicate_triple = Triple.new(1,2,-3)
+		end
+		specify { @triple.is_equal_to?(@triple).should eql true }
+		specify { @zero_triple.is_equal_to?(@zero_triple).should eql true }
+		specify { @negative_triple.is_equal_to?(@negative_triple).should eql true }
 
-	it "should multiply a triple by a positive constant" do
-		@triple.times_constant(10).y.should eql 20.0
-		@triple.times_constant(100).z.should eql -300.0
-		@negative_triple.times_constant(1).y.should eql -0.9
-		@zero_triple.times_constant(5).x.should eql 0.0
-	end
+		specify { @triple.is_equal_to?(Triple.new(1,2,-3)).should eql true }
+		specify { @triple.is_equal_to?(@duplicate_triple).should eql true }
 
-	it "should multiply a triple by a negative constant" do
-		@triple.times_constant(-10).y.should eql -20.0
-		@triple.times_constant(-100).z.should eql 300.0
-		@negative_triple.times_constant(-1).y.should eql 0.9
-		@zero_triple.times_constant(-5).x.should eql 0.0
+		specify { @triple.is_equal_to?(@negative_triple).should eql false }
+		specify { @zero_triple.is_equal_to?(@triple).should eql false }
+		specify { @negative_triple.is_equal_to?(Triple.new(1,2,3)).should eql false }
 	end
-
-	it "should multiply a triple by zero" do
-		@triple.times_constant(0).x.should eql 0.0
-		@triple.times_constant(0.0).z.should eql 0.0
-		@negative_triple.times_constant(0).y.should eql 0.0
-		@zero_triple.times_constant(0.0).x.should eql 0.0
-	end
-
-	it "should return true if a triple is compared to itself" do
-		@triple.is_equal_to?(@triple).should eql true
-		@zero_triple.is_equal_to?(@zero_triple).should eql true
-		@negative_triple.is_equal_to?(@negative_triple).should eql true
-	end
-
-	it "should return false if a triple is compared to another triple" do
-		@triple.is_equal_to?(@negative_triple).should eql false
-		@zero_triple.is_equal_to?(@triple).should eql false
-		@negative_triple.is_equal_to?(Triple.new(1,2,3)).should eql false
-	end
-
-	it "should return true if a triple is compared to a copy of itself" do
-		duplicate_triple = Triple.new(1,2,-3)
-		@triple.is_equal_to?(Triple.new(1,2,-3)).should eql true
-		@triple.is_equal_to?(duplicate_triple).should eql true
-	end
-
 
 end
