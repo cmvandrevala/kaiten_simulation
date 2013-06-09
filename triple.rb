19e3857e71f14212e360e002ce055594c617b36f
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-06-09 18:31:25 -0400

    Clean up triple.rb

--- triple.rb
+++ triple.rb
@@ -2,18 +2,15 @@ class Triple
 
 	attr_accessor :x, :y, :z
 
-
 	def initialize(x,y,z)
 		@x = x.to_f
 		@y = y.to_f
 		@z = z.to_f
 	end
 
-
 	def magnitude
 		Math.sqrt(@x**2 + @y**2 + @z**2)
 	end
-
 
 	def r_hat
 		if self.magnitude == 0.0
@@ -23,14 +20,12 @@ class Triple
 		end
 	end
 
-
 	def add_to(another_triple)
 		x_value = self.x + another_triple.x
 		y_value = self.y + another_triple.y
 		z_value = self.z + another_triple.z
 		return Triple.new(x_value, y_value, z_value)
 	end
-
 
 	def times_constant(constant)
 		x_value = constant*self.x
