7e8144d6449d472e1bfa2edc3994d48daee5189f
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-06-09 18:36:14 -0400

    Clean up kunai_spec.rb and kunai.rb

--- spec/kunai_spec.rb
+++ spec/kunai_spec.rb
@@ -4,30 +4,16 @@ describe Kunai do
 	
 	before :each do
 		@kunai = Kunai.new
+		@default_position = Triple.new(20.0, 0.0, 0.0)
+		@default_velocity = Triple.new(-25.0, 0.0, 0.0)
 	end
 
-	it "should be an instance of the class Kunai" do
-		@kunai.should be_an_instance_of Kunai
-	end
-
-	it "should have a default cross sectional area of 10 cm^2" do
-		@kunai.area.should eql 0.01
-	end
-
-	it "should have a default position of (20.0, 0.0, 0.0)" do
-		@kunai.position.x.should eql 20.0
-		@kunai.position.y.should eql 0.0
-		@kunai.position.z.should eql 0.0
-	end
-
-	it "should have a default mass of 1.0 kg" do
-		@kunai.mass.should eql 1.0
-	end
-
-	it "should have a default speed of (-25.0, 0.0, 0.0)" do
-		@kunai.velocity.x.should eql -25.0
-		@kunai.velocity.y.should eql 0.0
-		@kunai.velocity.z.should eql 0.0
+	context "it should initialize properly" do
+		it { should be_an_instance_of Kunai }
+		specify { @kunai.area.should eql 0.01 }
+		specify { @kunai.mass.should eql 1.0 }
+		specify { @kunai.position.is_equal_to?(@default_position).should eql true }
+		specify { @kunai.velocity.is_equal_to?(@default_velocity).should eql true }
 	end
 
 end
