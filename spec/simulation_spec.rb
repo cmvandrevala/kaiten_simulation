bc610a0399a299429dcbb7c67e3fcb42c7fc2be1
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-06-09 20:50:56 -0400

    Clean up simulation_spec.rb

--- spec/simulation_spec.rb
+++ spec/simulation_spec.rb
@@ -4,7 +4,25 @@ describe Simulation do
 	
 	before(:each) do
 		@simulation = Simulation.new
+
+		@x_acceleration = Triple.new(1000, 0, 0)
+		@x_time_stepped_velocity = Triple.new(-24.0, 0.0, 0.0)
+		@x_time_stepped_position = Triple.new(19.9755, 0.0, 0.0)
+
+		@y_acceleration = Triple.new(0, 1000, 0)
+		@y_time_stepped_velocity = Triple.new(-25.0, 1.0, 0.0)
+		@y_time_stepped_position = Triple.new(19.975, 0.0005, 0.0)
+
+		@z_acceleration = Triple.new(0, 0, 1000)
+		@z_time_stepped_velocity = Triple.new(-25.0, 0.0, 1.0)
+		@z_time_stepped_position = Triple.new(19.975, 0.0, 0.0005)
+
+		@acceleration = Triple.new(1000, 1000, 1000)
+		@time_stepped_velocity = Triple.new(-24.0, 1.0, 1.0)
+		@time_stepped_position = Triple.new(19.9755, 0.0005, 0.0005)
+
 		@zero_vector = Triple.new(0, 0, 0)
+		@zero_time_stepped_position = Triple.new(19.975, 0, 0)
 	end
 
 	context "when a simulation is created" do
@@ -22,91 +40,60 @@ describe Simulation do
 		specify { @simulation.calculate_force(Triple.new(12,0,0)).is_equal_to?(@zero_vector).should eql true }
 	end
 
-	context "when calculating the velocity in the next time step" do
+	context "when calculating the velocity in the next time step using @zero_vector" do
 		before(:each) do
-			@acceleration = Triple.new(1000, 1000, 1000)
-			@answer = Triple.new(-24.0, 1.0, 1.0)
+			@simulation.evolve_position(@zero_vector)
+			@simulation.evolve_velocity(@zero_vector)
+		end
+		specify { @simulation.kunai.velocity.should be_an_instance_of Triple }
+		specify { @simulation.kunai.position.should be_an_instance_of Triple }
+		specify { @simulation.kunai.velocity.is_equal_to?(@simulation.kunai.velocity).should eql true }
+		specify { @simulation.kunai.position.is_equal_to?(@zero_time_stepped_position).should eql true }
+	end
+
+	context "when calculating the velocity in the next time step using @acceleration" do
+		before(:each) do
+			@simulation.evolve_position(@acceleration)
 			@simulation.evolve_velocity(@acceleration)
 		end
 		specify { @simulation.kunai.velocity.should be_an_instance_of Triple }
-		specify { @simulation.kunai.velocity.is_equal_to?(@answer).should eql true }
+		specify { @simulation.kunai.position.should be_an_instance_of Triple }
+		specify { @simulation.kunai.velocity.is_equal_to?(@time_stepped_velocity).should eql true }
+		specify { @simulation.kunai.position.is_equal_to?(@time_stepped_position).should eql true }
 	end
 
-
-
-	it "should be able to calculate the velocity at the next time step for no acceleration" do
-		@simulation.kunai.velocity.x.should eql -25.0
-		@simulation.evolve_velocity(Triple.new(0, 0, 0))
-		@simulation.kunai.velocity.x.should eql -25.0
+	context "when calculating the parameters in the next time step using @x_acceleration" do
+		before(:each) do
+			@simulation.evolve_position(@x_acceleration)
+			@simulation.evolve_velocity(@x_acceleration)
+		end
+		specify { @simulation.kunai.velocity.should be_an_instance_of Triple }
+		specify { @simulation.kunai.position.should be_an_instance_of Triple }
+		specify { @simulation.kunai.velocity.is_equal_to?(@x_time_stepped_velocity).should eql true }
+		specify { @simulation.kunai.position.is_equal_to?(@x_time_stepped_position).should eql true }
 	end
 
-	it "should be able to calculate the velocity at the next time step for x-acceleration" do
-		@simulation.kunai.velocity.x.should eql -25.0
-		@simulation.evolve_velocity(Triple.new(1000, 0, 0))
-		@simulation.kunai.velocity.x.should eql -24.0
+	context "when calculating the parameters in the next time step using @y_acceleration" do
+		before(:each) do
+			@simulation.evolve_position(@y_acceleration)
+			@simulation.evolve_velocity(@y_acceleration)
+		end
+		specify { @simulation.kunai.velocity.should be_an_instance_of Triple }
+		specify { @simulation.kunai.position.should be_an_instance_of Triple }
+		specify { @simulation.kunai.velocity.is_equal_to?(@y_time_stepped_velocity).should eql true }
+		specify { @simulation.kunai.position.is_equal_to?(@y_time_stepped_position).should eql true }
 	end
 
-	it "should be able to calculate the velocity at the next time step for y-acceleration" do
-		@simulation.kunai.velocity.y.should eql 0.0
-		@simulation.evolve_velocity(Triple.new(0, 1000, 0))
-		@simulation.kunai.velocity.y.should eql 1.0
+	context "when calculating the parameters in the next time step using @z_acceleration" do
+		before(:each) do
+			@simulation.evolve_position(@z_acceleration)
+			@simulation.evolve_velocity(@z_acceleration)
+		end
+		specify { @simulation.kunai.velocity.should be_an_instance_of Triple }
+		specify { @simulation.kunai.position.should be_an_instance_of Triple }
+		specify { @simulation.kunai.velocity.is_equal_to?(@z_time_stepped_velocity).should eql true }
+		specify { @simulation.kunai.position.is_equal_to?(@z_time_stepped_position).should eql true }
 	end
-
-	it "should be able to calculate the velocity at the next time step for z-acceleration" do
-		@simulation.kunai.velocity.z.should eql 0.0
-		@simulation.evolve_velocity(Triple.new(0, 0, 1000))
-		@simulation.kunai.velocity.z.should eql 1.0
-	end
-
-	it "should be able to calculate the velocity at the next time step for a general acceleration" do
-		@simulation.kunai.velocity.x.should eql -25.0
-		@simulation.kunai.velocity.y.should eql 0.0
-		@simulation.kunai.velocity.z.should eql 0.0
-		@simulation.evolve_velocity(Triple.new(1000, 1000, 1000))
-		@simulation.kunai.velocity.x.should eql -24.0
-		@simulation.kunai.velocity.y.should eql 1.0
-		@simulation.kunai.velocity.z.should eql 1.0
-	end
-
-	it "should be able to calculate the next position for no acceleration" do
-		@simulation.kunai.position.x.should eql 20.0
-		@simulation.evolve_position(Triple.new(0, 0, 0))
-		@simulation.kunai.position.x.should eql 19.975
-	end
-
-	it "should be able to calculate the next position for x-acceleration" do
-		@simulation.kunai.position.x.should eql 20.0
-		@simulation.evolve_position(Triple.new(1000, 0, 0))
-		@simulation.kunai.position.x.should eql 19.9755
-	end
-
-	it "should be able to calculate the next position for y-acceleration" do
-		@simulation.kunai.position.y.should eql 0.0
-		@simulation.evolve_position(Triple.new(0, 1000, 0))
-		@simulation.kunai.position.y.should eql 0.0005
-	end
-
-	it "should be able to calculate the next position for z-acceleration" do
-		@simulation.kunai.position.z.should eql 0.0
-		@simulation.evolve_position(Triple.new(0, 0, 1000))
-		@simulation.kunai.position.z.should eql 0.0005
-	end
-
-	it "should be able to calculate the next position for a general acceleration" do
-		@simulation.kunai.position.x.should eql 20.0
-		@simulation.kunai.position.y.should eql 0.0
-		@simulation.kunai.position.z.should eql 0.0
-		@simulation.evolve_position(Triple.new(1000, 1000, 1000))
-		@simulation.kunai.position.x.should eql 19.9755
-		@simulation.kunai.position.y.should eql 0.0005
-		@simulation.kunai.position.z.should eql 0.0005
-	end
-
-
-
-
-
-
 
 	#create a method called calculate_acceleration where F = ma
 
