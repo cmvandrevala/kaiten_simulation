7b60497334856b44c4e8220503810742ba68c487
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-07-18 20:12:08 -0500

    Update all tests to confirm with current rspec syntax

--- spec/kaiten_spec.rb
+++ spec/kaiten_spec.rb
@@ -3,53 +3,113 @@ require "vector"
 
 describe Kaiten do
 
-	before :all do
-		@kaiten = Kaiten.new(5)
-		@another_kaiten = Kaiten.new(5, 12, 13)
-		@center = Vector[0, 0, 0]
-		@x_edge = Vector[5, 0, 0]
-		@x_answer = Vector[1, 5, 0]
-		@y_edge = Vector[0, 5, 0]
-		@y_answer = Vector[-5, 1, 0]
-		@z_edge = Vector[0, 0, 5]
-		@z_answer = Vector[0, 0, 1]
-		@barely_outside = Vector[5.00001, 0.0, 0.0]
-		@far_away_one_direction = Vector[0, 1000, 0]
-		@far_away_xyz = Vector[100, 236, 129]
-		@edge_point = Vector[5/Math.sqrt(2), 5/Math.sqrt(2), 0]
-		@edge_answer = Vector[-4/Math.sqrt(2), 6/Math.sqrt(2), 0]
-		@second_edge_point = Vector[1, 1, Math.sqrt(23) ]
-		@second_edge_answer = Vector[-0.8, 1.2, Math.sqrt(23)/5]
+	context "default settings" do
+    
+    before(:each) do
+      @kaiten = Kaiten.new(5)
+    end
+    
+    it "has a default radius" do
+		  expect(@kaiten.radius).to eql 5.0
+    end
+    
+    it "has a default chakra angular speed" do
+		  expect(@kaiten.chakra_angular_speed).to eql 1.0
+    end
+    
+    it "has a default air density" do
+		  expect(@kaiten.air_density).to eql 1.225
+    end
+    
+    it "has a default radial chakra velocity" do
+		  expect(@kaiten.outward_chakra_speed).to eql 1.0
+    end
+    
 	end
 
-	context "it should initialize a default instance of the class Kaiten" do
-		specify { @kaiten.should be_an_instance_of Kaiten }
-		specify { @kaiten.radius.should eql 5.0 }
-		specify { @kaiten.radius.should be_an_instance_of Float }
-		specify { @kaiten.chakra_angular_speed.should eql 1.0 }
-		specify { @kaiten.chakra_angular_speed.should be_an_instance_of Float }
-		specify { @kaiten.air_density.should eql 1.225 }
-		specify { @kaiten.air_density.should be_an_instance_of Float }
-		specify { @kaiten.outward_chakra_speed.should eql 1.0 }
-		specify { @kaiten.outward_chakra_speed.should be_an_instance_of Float }
+	context "with parameters" do
+    
+    before(:each) do
+      @another_kaiten = Kaiten.new(6, 12, 13)
+    end
+
+    it "has a radius" do
+		  expect(@another_kaiten.radius).to eql 6.0
+    end
+    
+    it "has a chakra angular speed" do
+		  expect(@another_kaiten.chakra_angular_speed).to eql 12.0
+    end
+    
+    it "has an air density" do
+		  expect(@another_kaiten.air_density).to eql 1.225
+    end
+    
+    it "has a radial chakra velocity" do
+		  expect(@another_kaiten.outward_chakra_speed).to eql 13.0
+    end
+    
 	end
 
-	context "it should initialize an instance of the class Kaiten with parameters" do
-		specify { @another_kaiten.chakra_angular_speed.should eql 12.0 }
-		specify { @another_kaiten.chakra_angular_speed.should be_an_instance_of Float }
-		specify { @another_kaiten.outward_chakra_speed.should eql 13.0 }
-		specify { @another_kaiten.outward_chakra_speed.should be_an_instance_of Float }
-	end
-
-	context "the method total_chakra_velocity should return the total chakra velocity at a point" do
-		specify { @kaiten.total_chakra_velocity(@x_edge).should == @x_answer }
-		specify { @kaiten.total_chakra_velocity(@y_edge).should == @y_answer }
-		specify { @kaiten.total_chakra_velocity(@z_edge).should == @z_answer }
-		specify { @kaiten.total_chakra_velocity(@barely_outside).magnitude.should eql 0.0 }
-		specify { @kaiten.total_chakra_velocity(@far_away_one_direction).magnitude.should eql 0.0 }
-		specify { @kaiten.total_chakra_velocity(@far_away_xyz).magnitude.should eql 0.0 }
-		specify { @kaiten.total_chakra_velocity(@edge_point).should == @edge_answer }
-		specify { @kaiten.total_chakra_velocity(@second_edge_point).should == @second_edge_answer }
+	context "chakra velocity at the edges of the kaiten" do
+    
+    before(:each) do
+      @kaiten = Kaiten.new(5)
+  		@x_edge = Vector[5, 0, 0]
+  		@y_edge = Vector[0, 5, 0]
+  		@z_edge = Vector[0, 0, 5]
+    end
+    
+    it "calculates it at the x-edge" do
+		  expect(@kaiten.total_chakra_velocity(@x_edge)).to eql Vector[1.0, 5.0, 0.0]
+    end
+    
+    it "calculates it at the y-edge" do
+		  expect(@kaiten.total_chakra_velocity(@y_edge)).to eql Vector[-5.0, 1.0, 0.0]
+    end
+    
+    it "calculates it at the z-edge" do
+		  expect(@kaiten.total_chakra_velocity(@z_edge)).to eql Vector[0.0, 0.0, 1.0]
+    end
+    
+  end
+  
+  context "chakra velocity outside of the kaiten" do
+    
+    before(:each) do
+      @kaiten = Kaiten.new(5)
+  		@barely_outside = Vector[5.00001, 0.0, 0.0]
+  		@far_away_one_direction = Vector[0, 1000, 0]
+  		@far_away_xyz = Vector[100, 236, 129]
+    end
+    
+    it "calculates it barely outside the kaiten" do
+      expect(@kaiten.total_chakra_velocity(@barely_outside).magnitude).to eql 0.0
+    end
+    
+    it "calculates it far away along one direction" do
+      expect(@kaiten.total_chakra_velocity(@far_away_one_direction).magnitude).to eql 0.0
+    end
+    
+    it "calculates it far away along many directions" do
+      expect(@kaiten.total_chakra_velocity(@far_away_xyz).magnitude).to eql 0.0
+    end
+    
+  end
+  
+  context "chakra velocity at arbitrary points" do
+    
+  	before(:each) do
+      @kaiten = Kaiten.new(5)
+  		@point = Vector[5/Math.sqrt(2), 5/Math.sqrt(2), 0]
+  	end
+  
+		it "calculates the chakra velocity at some point" do
+      expect(@kaiten.total_chakra_velocity(@point).x).to eql -2.82842712474619
+      expect(@kaiten.total_chakra_velocity(@point).y).to eql 4.242640687119285
+      expect(@kaiten.total_chakra_velocity(@point).z).to eql 0.0   
+    end
+    
 	end
 
 end
